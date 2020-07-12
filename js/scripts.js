1c0cad9c1dac0f2c6ed179ee51ce641e7a19dd8e
diogenesjup <diogenesjunior.ti@gmail.com>
2020-07-12 11:45:38 -0300

    criação do backup inicial

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+                        $( window ).ready(function() {
+
+                                var superBanner = $('#superBanner').owlCarousel({
+                                        loop:true,
+                                        margin:0,
+                                        items: 1,
+                                        autoplay: true,
+                                        center: true,
+                                        //navText: [
+                                        //    '<img src="images/left.png" alt="Banner anterior">',
+                                        //    '<img src="images/right.png" alt="Próximo Banner">'
+                                        //], 
+                                        //navContainer: '.custom-nav-banner',
+                                        autoplayTimeout:2500,
+                                        //dotsContainer: '#carousel-custom-dots',
+                                        autoplayHoverPause:true,
+                                        //animateIn: 'fadeIn', // add this
+                                        //animateOut: 'fadeOut', // and this
+                                       
+                                });
+                        }); 
